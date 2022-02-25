831d0a1dbbf029ba280adb2d3a80dee69df02b8d
Joubertp <pierrejoubert883@gmail.com>
2022-02-25 18:56:18 +0100

    ajout de la rout put users, permettant de modifier un user

--- back-app-securiface/src/main/java/com/example/demo/web/UserController.java
+++ back-app-securiface/src/main/java/com/example/demo/web/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,6 +47,18 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
 		return new ResponseEntity<>(opUser.get(), HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<User> update(@RequestBody User toUpUser) {
+		Optional<User> opUser = userRep.findById(toUpUser.getId());
+
+		if (!opUser.isPresent())
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+		User newUser = userRep.save(toUpUser);
+
+		return new ResponseEntity<>(newUser, HttpStatus.OK);
 	}
 
 	@GetMapping("/login")
